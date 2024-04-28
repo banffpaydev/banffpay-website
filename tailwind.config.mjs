d9a576efbfcf6cf9e0a1cc0b6ca0900bf63cd5ed
Abdullah Solahudeen <solahudeen15@gmail.com>
2024-04-28 18:41:08 +0100

    Add Astro configuration and update dependencies

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+import defaultTheme from "tailwindcss/defaultTheme"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Onest Variable", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "brand-primary": {
+                    50: "#FAFDFF",
+                    100: "#F0F9FF",
+                    200: "#E6F5FF",
+                    300: "#CCEAFF",
+                    400: "#B3E0FF",
+                    500: "#99D5FF",
+                    600: "#80CBFF",
+                    700: "#67C1FF",
+                    800: "#4DB6FF",
+                    900: "#34ACFF",
+                    950: "#1AA1FF",
+                },
+            },
+        },
+    },
+    plugins: [],
+}
