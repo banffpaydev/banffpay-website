5511fd2a3bf6e1e27f6a70e559b7568302923a89
Abdullah Solahudeen <solahudeen15@gmail.com>
2024-04-29 14:25:47 +0100

    Update privacy page

---
+++ src/components/privacyContent.js
@@ -0,0 +1,67 @@
+export const data = [
+    {
+        title: "Introduction",
+        id: "introduction",
+    },
+    {
+        title: "Consent",
+        id: "consent",
+    },
+    {
+        title: "Notice",
+        id: "notice",
+    },
+    {
+        title: "Information We Collect",
+        id: "information-we-collect",
+    },
+    {
+        title: "How We Collect Your Personal Information",
+        id: "how-we-collect",
+    },
+    {
+        title: "How We Use Your Information We Collect",
+        id: "how-we-use",
+    },
+    {
+        title: "Data Sharing",
+        id: "data-sharing",
+    },
+    {
+        title: "Log Files",
+        id: "log-files",
+    },
+    {
+        title: "Cookies and Monitoring Mechanisms",
+        id: "cookies",
+    },
+    {
+        title: "Information Security",
+        id: "information-security",
+    },
+    {
+        title: "Third Party Privacy Policies",
+        id: "third-party",
+    },
+    {
+        title: "A Special Note About Children Information",
+        id: "children-information",
+    },
+    {
+        title: "Access And Corrections",
+        id: "access-and-corrections",
+    },
+    {
+        title: "How Long We Keep My Personal Information?",
+        id: "how-long-we-keep",
+    },
+    {
+        title: "Policy Changes",
+        id: "policy-changes",
+    },
+    {
+        title: "Get In Touch",
+        content: "Fugiat nulla pariatur",
+        id: "get-in-touch",
+    },
+]
