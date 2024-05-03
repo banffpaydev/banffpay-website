cf4cf97cd08cbd970bb5dbd96bbdc25a653a3838
Abdullah Solahudeen <solahudeen15@gmail.com>
2024-05-03 08:02:03 +0100

    Update ListButton and Footer components

--- src/components/content.js
+++ src/components/content.js
@@ -1,4 +1,4 @@
-export const data = [
+export const privacyData = [
     {
         title: "Introduction",
         id: "introduction",
@@ -61,7 +61,65 @@ export const data = [
     },
     {
         title: "Get In Touch",
-        content: "Fugiat nulla pariatur",
         id: "get-in-touch",
+    },
+]
+
+export const termsData = [
+    {
+        title: "Introduction",
+        id: "introduction",
+    },
+    {
+        title: "General Information",
+        id: "general-information",
+    },
+    {
+        title: "Registration",
+        id: "registration",
+    },
+    {
+        title: "Eligibility",
+        id: "eligibility",
+    },
+    {
+        title: "Liabilities",
+        id: "liabilities",
+    },
+    {
+        title: "Reward",
+        id: "reward",
+    },
+    {
+        title: "Charges and currency exchange",
+        id: "charges-and-currency",
+    },
+    {
+        title: "Restriction on Maximum Transaction value",
+        id: "restriction",
+    },
+    {
+        title: "Data Protection and Privacy",
+        id: "data-protection",
+    },
+    {
+        title: "Termination and Enforcement of action",
+        id: "termination",
+    },
+    {
+        title: "Disclaimer",
+        id: "disclaimer",
+    },
+    {
+        title: "Cookies",
+        id: "cookies",
+    },
+    {
+        title: "Intellectual Property Rights",
+        id: "intellectual-property",
+    },
+    {
+        title: "How We are Regulated",
+        id: "how-regulated",
     },
 ]
